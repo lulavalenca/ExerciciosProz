149cbdcb8aa2aca43dabdb6b576b3c0698f8e5c5
Luiz Valença Filho <lulavalencafilho@gmail.com>
2023-07-30 09:11:16 -0300

    exercicio inicial

---
+++ ProzEx1.py
@@ -0,0 +1,11 @@
+# Utilizando o laço for em ordem crescente
+print("Utilizando o laço 'for' em ordem crescente:")
+for numero in range(1, 21):
+    if numero != 13:
+        print(numero)
+
+# Utilizando o laço for em ordem decrescente
+print("\nUtilizando o laço 'for' em ordem decrescente:")
+for numero in range(20, 0, -1):
+    if numero != 13:
+        print(numero)
